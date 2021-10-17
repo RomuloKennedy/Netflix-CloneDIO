0f3ff91962c3b78f050541b6fe8372e557dd7f5f
RomuloKennedy <82661711+RomuloKennedy@users.noreply.github.com>
2021-10-17 14:48:59 -0300

    segundo commit

---
+++ js/main.js
@@ -0,0 +1,32 @@
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+
+function redirectSeries(){
+    window.open("https://www.oficinadanet.com.br/post/16636-as-10-melhores-series-para-assistir-na-netflix-segundo-nota-do-imdb");
+}
+function redirectMovie(){
+    window.open("https://www.oficinadanet.com.br/netflix2018/23817-os-melhores-filmes-para-assistir-na-netflix-em-2018");
+}
+function redirectDocumentary(){
+    window.open("https://www.oficinadanet.com.br/netflix/31103-os-melhores-documentarios-para-ver-na-netflix");
+}
+function redirectWatch(){
+    window.open("https://api.animesgratisbr.com/video/8685");
+}
+function redirectInfo(){
+    window.open("https://onepiece.fandom.com/pt/wiki/One_Piece_(Anime)");
+}

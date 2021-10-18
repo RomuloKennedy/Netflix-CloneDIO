6daca3fa324f3e8ccee945ac2276cc1828a616e7
RomuloKennedy <82661711+RomuloKennedy@users.noreply.github.com>
2021-10-18 12:32:31 -0300

    trocando link de assistir.
    o link antigo tinha expirado.

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ function redirectDocumentary(){
     window.open("https://www.oficinadanet.com.br/netflix/31103-os-melhores-documentarios-para-ver-na-netflix");
 }
 function redirectWatch(){
-    window.open("https://rr2---sn-j5cvnxg8pjvn-o5us.googlevideo.com/videoplayback?expire=1634514941&ei=fUdsYcT7A_KG_9EPx8el6A0&ip=193.70.57.76&id=4e6c448885755a8d&itag=18&source=blogger&susc=bl&mime=video/mp4&vprv=1&dur=1500.264&lmt=1549822062143798&sparams=expire,ei,ip,id,itag,source,susc,mime,vprv,dur,lmt&sig=AOq0QJ8wRAIgUwx50Cy2731KdiAags-dBA_Z8tfSxZIqb0jUUjPoeGICICGwAwWnBp7rubV9MJSU-4jch3qnPFSseU7fXJa6enrR&redirect_counter=1&rm=sn-25gkr7l&req_id=59541cd291d3a3ee&cms_redirect=yes&ipbypass=yes&mh=0c&mip=177.89.137.107&mm=31&mn=sn-j5cvnxg8pjvn-o5us&ms=au&mt=1634502294&mv=m&mvi=2&pl=21&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRAIgKTwG_EQKWVths2RNljoyFI5ShgfSzNB3afZaWprUN2ACIA1jbfX2rwwB413i3U2eEEMX0JBTrpWHSSv0hEYfJPIt");
+    window.open("https://www.crunchyroll.com/pt-br/one-piece");
 }
 function redirectInfo(){
     window.open("https://onepiece.fandom.com/pt/wiki/One_Piece_(Anime)");
